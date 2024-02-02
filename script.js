251ef080ec3b0e250e1442291e10f22c72c20d59
sahilmate <matesahil04@gmail.com>
2024-02-02 19:06:02 +0530

    Refactor code: Improve formatting and add missing semicolons

--- script.js
+++ script.js
@@ -1,16 +1,11 @@
 const Heart_button = document.getElementsByClassName("btn-heart");
 
-
-function toggleButton(e){
-    e.classList.toggle("active")
-
+function toggleButton(e) {
+  e.classList.toggle("active");
 }
 
-
-for(let i = 0;i<Heart_button.length;i++)
-{
-    Heart_button[i].addEventListener("click",function(){
-        toggleButton(this);
-    })
-
+for (let i = 0; i < Heart_button.length; i++) {
+  Heart_button[i].addEventListener("click", function () {
+    toggleButton(this);
+  });
 }
